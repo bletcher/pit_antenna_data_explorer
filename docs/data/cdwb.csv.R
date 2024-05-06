bae8649889e5df110c4bbcd102f1a597fc8b08a3
Ben Letcher <bletcher@usgs.gov>
2024-05-06 13:25:34 -0400

    take data files out of .gitignore

---
+++ docs/data/cdwb.csv.R
@@ -0,0 +1,25 @@
+library(readr)
+library(jsonlite)
+library(tidyverse)
+
+# loads 'all'
+load("./docs/data/cdWB_all.RData")
+
+# create smaller dataset for the app
+d <- all |>
+  dplyr::select(
+    species, tag, detectionDate, lagDetectionDate, year, riverOrdered,
+    observedLength, observedWeight, survey, cohort, sectionN, riverMeter, aliveOrDead, nPerInd, readerId
+  ) |>
+  filter(species %in% c("bkt", "bnt", "ats")) |>
+  # mutate(dateDiff = lagDetectionDate - detectionDate) |>
+  mutate(
+    tag = ifelse(is.na(tag), "untagged", tag),
+    nPerInd = ifelse(tag == "untagged", 1, nPerInd)
+    ) |>
+  group_by(tag) |>
+    arrange(detectionDate) |>
+  ungroup()
+
+# write_json(d, "./docs/data/all_for_obs.json") # this is 98 mB vs 60 for csv 
+write.csv(d, "./docs/data/all_for_obs.csv")
